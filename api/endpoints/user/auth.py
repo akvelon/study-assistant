c8a437cd6d531c1c2da34db7133aba85115a6a30
Kirill Ustinov <kirill.ustinov@akvelon.com>
2023-07-14 20:19:08 +0000

    SIACP-67: address auth todos

--- api/endpoints/user/auth.py
+++ api/endpoints/user/auth.py
@@ -1,16 +1,16 @@
 import sqlite3
-from fastapi import APIRouter, Response, HTTPException
+from fastapi import APIRouter, Response, HTTPException, status
 from pydantic import BaseModel
-import os
+import re
 
-from api.endpoints.user import user_router, User, get_token
+from api.endpoints.user import user_router, User, get_token, check_email
 from api.db.users import users_db, get_pass_hash
 
 
 class UserRequest(BaseModel):
     email: str
     password: str
-    schoolId: int = 0   
+    schoolId: int = 0
 
 class UserResponse(User):
     pass
@@ -23,35 +23,47 @@ class AuthResponse(BaseModel):
     token: str
     user: UserResponse
 
-
-def check_email(email: str):
-    # TODO: implement email checking
-    # Should raise HTTPException with proper status code
-    pass
-
 @user_router.post("/auth")
 async def authenticate_user(credentials: AuthRequest) -> AuthResponse:
     try:
+        credentials_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+        existence_exception = HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User does not exist",
+        )
+
         check_email(credentials.email)
         user = users_db.get_user(credentials.email)
 
-        # TODO: check if user is None
-
-        if get_pass_hash(credentials.password, user.salt) == user.password_hash:
-            return AuthResponse(token=user.token, user=UserResponse(email=user.email, schoolId=user.school_id))
+        if user is None:
+            raise existence_exception
         else:
-            # TODO: use status from fastapi e.g. status.500_...
-            raise HTTPException(status_code=401, detail="Invalid Credentials")
+            if get_pass_hash(credentials.password, user.salt) == user.password_hash:
+                return AuthResponse(token=user.token, user=UserResponse(id=user.id, email=user.email, schoolId=user.school_id))
+            else:
+                raise credentials_exception
 
     except sqlite3.Error as error:
+        connection_exception = HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Could not connect to Users Database",
+        )
         print("Failed to connect to sqlite3 database, error: ", error)
-        # TODO: use status from fastapi e.g. status.500_...
-        raise HTTPException(status_code=500, detail="Could not connect to Users Database")
+        raise connection_exception
 
 
 @user_router.post('/register', status_code=201)
 async def register_user(user: UserRequest) -> AuthResponse:
     try:
+        duplicate_exception = HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="User already exists",
+        )
+
         db_user = users_db.get_user(user.email)
         if db_user is None:
             check_email(user.email)
@@ -63,12 +75,14 @@ async def register_user(user: UserRequest) -> AuthResponse:
 
             db_user = users_db.get_user(user.email)
     
-            return AuthResponse(token=token, user=UserResponse(email=db_user.email, schoolId=db_user.school_id))
+            return AuthResponse(token=token, user=UserResponse(id=db_user.id, email=db_user.email, schoolId=db_user.school_id))
         else:
-            # TODO: use status from fastapi e.g. status.500_...
-            raise HTTPException(status_code=409, detail="User already exists")
+            raise duplicate_exception
 
     except sqlite3.Error as error:
+        connection_exception = HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Could not connect to Users Database",
+        )
         print("Failed to connect to sqlite3 database, error: ", error)
-        # TODO: use status from fastapi e.g. status.500_...
-        raise HTTPException(status_code=500, detail="Could not connect to Users Database")
+        raise connection_exception
