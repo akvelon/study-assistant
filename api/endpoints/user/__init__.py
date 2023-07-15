35e4f877dde8dd2f99e3c9f372972c0e8d114e45
Evgenii Olonov <evgenii.olonov@akvelon.com>
2023-07-15 02:19:08 +0000

    Revert "Merge branch 'SIACP-64' into 'dev'"

--- api/endpoints/user/__init__.py
+++ api/endpoints/user/__init__.py
@@ -3,15 +3,12 @@ from fastapi import APIRouter, Depends, Security, HTTPException, status
 from fastapi.security.api_key import APIKeyHeader
 from pydantic import BaseModel
 from typing_extensions import Annotated
-from api.db.users import UsersDB
-from api.endpoints.user.auth import check_email
 
 secret_key = "temporary_secret_key_73857982"
 
 user_router = APIRouter(prefix='/user', tags=[""])
 
 class User(BaseModel):
-    id: int
     email: str = ""
     schoolId: int = 0
 
@@ -21,11 +18,6 @@ def get_token(id, email):
         "exp": None
     }
     return jwt.encode(payload, secret_key, algorithm="HS256")
-
-class TokenData(BaseModel):
-    id: int
-    email: str
-
 
 # FIXME: the method doesn't work, needs debugging and fixing
 async def get_current_user(token: str = Security(APIKeyHeader(name="Authorization"))):
@@ -40,19 +32,16 @@ async def get_current_user(token: str = Security(APIKeyHeader(name="Authorizatio
         user_data = payload.get("sub")
         if user_data is None:
             raise credentials_exception
-        token_data = TokenData(user_data)
-
+        token_data = TokenData()
     # TODO: refactor exceptions handling
     except Exception as error:
         print(error)
         raise credentials_exception
-
-    check_email(token_data.email)
-    db_user = UsersDB.get_user(token_data.email)
-    if db_user is None:
-        raise credentials_exception
-
-    return User(id=db_user.id, email=db_user.email, schoolId=db_user.school_id)
+    # TODO: fetch user from DB
+    # TODO: handle user related errors e.g.
+    # if user is None:
+    #    raise credentials_exception
+    return user_data
 
 import api.endpoints.user.auth
 import api.endpoints.user.school
