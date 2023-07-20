0b41f0ef6340e1713e59ba3e5fdd6852a00c1651
Artem Halushka <artem.halushka@akvelon.com>
2023-07-20 15:36:17 +0000

    SIACP-72 Attachments follow-up work

--- api/endpoints/schemas.py
+++ api/endpoints/schemas.py
@@ -5,12 +5,14 @@ class MessageAttachment(BaseModel):
     title: str
     summary: str
     url: str
-    image: str
+    image: str = None
 
 class Message(BaseModel):
     id: str
     role: str
     timestamp: int
     content: str
-    attachments: list[MessageAttachment]
+    attachments: list[MessageAttachment] = None
 
+class SearchQuery(BaseModel):
+    messages: list[Message]
