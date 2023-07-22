65f30dcfb8cddd4538ea4670121ad9529809ef8d
Lyubomyr Kryshtanovskyi <l.kryshtanovskyi@akvelon.com>
2023-07-22 02:42:34 +0000

    SIACP-71: Indexing: fields metadata and image_metadata are not encoded to JSON

--- se_indexing/db_engine/db.py
+++ se_indexing/db_engine/db.py
@@ -4,6 +4,8 @@ from uuid import uuid4
 from pydantic import BaseModel
 import pickle
 import numpy as np
+import json
+from contextlib import closing
 
 
 class DocumentEntry(BaseModel):
@@ -16,22 +18,33 @@ class DocumentEntry(BaseModel):
     content: str
     summary: str
     embedding: None = None
-    def __init__(self, id, url, title, type, metadata, image_metadata, content, summary, embedding=None):
 
+    def __init__(
+        self,
+        id,
+        url,
+        title,
+        type,
+        metadata,
+        image_metadata,
+        content,
+        summary,
+        embedding=None,
+    ):
         super().__init__(
             id=id,
             url=url,
             title=title,
             type=type,
-            metadata=eval(metadata),
-            image_metadata=eval(image_metadata),
+            metadata=json.loads(metadata),
+            image_metadata=json.loads(image_metadata),
             content=content,
             summary=summary,
         )
         self.embedding = np.array(pickle.loads(embedding))
 
     class Config:
-        fields = {'embedding': {'exclude': True}}
+        fields = {"embedding": {"exclude": True}}
 
 
 class IndexDB:
@@ -39,20 +52,26 @@ class IndexDB:
         self.path = path
 
         self.connection = sqlite3.connect(path)
-        self.cursor = self.connection.cursor()
+
+    def __del__(self):
+        if self.connection:
+            self.connection.close()
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.cursor.close()
-        self.connection.close()
+        if self.connection:
+            self.connection.close()
 
     def delete_database(self):
         """WARNING: Deletes the entire indexing database!"""
-        self.cursor.execute("DROP TABLE IF EXISTS embeddings")
-        self.cursor.execute("DROP TABLE IF EXISTS summaries")
-        self.cursor.execute("DROP TABLE IF EXISTS documents")
+        with closing(self.connection.cursor()) as cursor:
+            cursor.execute("DROP TABLE IF EXISTS embeddings")
+            cursor.execute("DROP TABLE IF EXISTS summaries")
+            cursor.execute("DROP TABLE IF EXISTS documents")
+
+        self.connection.commit()
 
     def create_database_if_not_exists(self):
         """Creates the indexing database, if it doesn't exist already."""
@@ -79,39 +98,45 @@ class IndexDB:
             type text,
             embedding blob
         );"""
-        self.cursor.execute(sql_create_documents_table)
-        self.cursor.execute(sql_create_summaries_table)
-        self.cursor.execute(sql_create_embedding_table)
+        with closing(self.connection.cursor()) as cursor:
+            cursor.execute(sql_create_documents_table)
+            cursor.execute(sql_create_summaries_table)
+            cursor.execute(sql_create_embedding_table)
+
+        self.connection.commit()
 
     def get_documents(self):
-        self.cursor.execute("""
-            SELECT
-                documents.id,
-                documents.url,
-                documents.title,
-                documents.type,
-                documents.metadata,
-                documents.image_metadata,
-                documents.content,
-                summaries.summary,
-                embeddings.embedding
-            FROM
-                documents
-            INNER JOIN
-                summaries
-            ON
-                summaries.document_id = documents.id
-            INNER JOIN
-                embeddings 
-            ON 
-                embeddings.summary_id = summaries.id;
-        """)
-        rows = self.cursor.fetchall()
-        documents = []
-        for row in rows:
-            document = DocumentEntry(*row)
-            documents.append(document)
-        return documents
+        with closing(self.connection.cursor()) as cursor:
+            cursor.execute(
+                """
+                SELECT
+                    documents.id,
+                    documents.url,
+                    documents.title,
+                    documents.type,
+                    documents.metadata,
+                    documents.image_metadata,
+                    documents.content,
+                    summaries.summary,
+                    embeddings.embedding
+                FROM
+                    documents
+                INNER JOIN
+                    summaries
+                ON
+                    summaries.document_id = documents.id
+                INNER JOIN
+                    embeddings 
+                ON 
+                    embeddings.summary_id = summaries.id;
+            """
+            )
+            rows = cursor.fetchall()
+            documents = []
+            for row in rows:
+                document = DocumentEntry(*row)
+                documents.append(document)
+            return documents
 
     def insert_document(self, document):
         """Add document to indexing database, and return its ID."""
@@ -120,18 +145,19 @@ class IndexDB:
         INSERT INTO documents(id, url, title, type, metadata, image_metadata, content)
         VALUES(?, ?, ?, ?, ?, ?, ?)
         """
-        self.cursor.execute(
-            sql_insert_document,
-            (
-                str(document["id"]),
-                str(document["url"]),
-                str(document["title"]),
-                str(document["type"]),
-                str(document["metadata"]),
-                str(document["image_metadata"]),
-                str(document["content"]),
-            ),
-        )
+        with closing(self.connection.cursor()) as cursor:
+            cursor.execute(
+                sql_insert_document,
+                (
+                    document["id"],
+                    document["url"],
+                    document["title"],
+                    document["type"],
+                    json.dumps(document["metadata"]),
+                    json.dumps(document["image_metadata"]),
+                    document["content"],
+                ),
+            )
         self.connection.commit()
         return document["id"]
 
@@ -142,9 +168,8 @@ class IndexDB:
         VALUES(?, ?, ?)
         """
         summary_id = str(uuid4())
-        self.cursor.execute(
-            sql_insert_summary, (summary_id, str(document_id), str(summary))
-        )
+        with closing(self.connection.cursor()) as cursor:
+            cursor.execute(sql_insert_summary, (summary_id, document_id, summary))
         self.connection.commit()
         return summary_id
 
@@ -154,9 +179,11 @@ class IndexDB:
         INSERT INTO embeddings(id, summary_id, type, embedding)
         VALUES(?, ?, ?, ?)"""
         embedding_id = str(uuid4())
-        self.cursor.execute(
-            sql_insert_embedding, (embedding_id, str(summary_id), str(type), embedding)
-        )
+        with closing(self.connection.cursor()) as cursor:
+            cursor.execute(
+                sql_insert_embedding,
+                (embedding_id, summary_id, type, embedding),
+            )
         self.connection.commit()
         return embedding_id
 
@@ -164,17 +191,20 @@ class IndexDB:
         sql_find_document = """
         SELECT * FROM documents WHERE url = ?
         """
-        self.cursor.execute(sql_find_document, (url,))
-        return self.cursor.fetchone()
+        with closing(self.connection.cursor()) as cursor:
+            cursor.execute(sql_find_document, (url,))
+            return cursor.fetchone()
 
     def find_summary_by_document_id(self, document_id):
         sql_find_summary = """
         SELECT * FROM summaries WHERE document_id = ?"""
-        self.cursor.execute(sql_find_summary, (document_id,))
-        return self.cursor.fetchone()
+        with closing(self.connection.cursor()) as cursor:
+            cursor.execute(sql_find_summary, (document_id,))
+            return cursor.fetchone()
 
     def find_embedding_by_summary_id(self, summary_id):
         sql_find_embedding = """
         SELECT * FROM embeddings WHERE summary_id = ?"""
-        self.cursor.execute(sql_find_embedding, (summary_id,))
-        return self.cursor.fetchone()
+        with closing(self.connection.cursor()) as cursor:
+            cursor.execute(sql_find_embedding, (summary_id,))
+            return cursor.fetchone()
