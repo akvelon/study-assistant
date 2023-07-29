2fac2b8ce78b93db17c80da2ffc6230ff84a549e
Kirill Ustinov <kirill.ustinov@akvelon.com>
2023-07-29 02:12:08 +0000

    SIACP-18: Pagination support for history endpoint

--- api/endpoints/history.py
+++ api/endpoints/history.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter, Response, Depends, HTTPException
-from pydantic import BaseModel
+"""
+    History endpoint
+"""
 from typing import Annotated
-from settings import settings
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import Field
+from fastapi_pagination import Page, paginate
 
-from api.endpoints.schemas import Message, HistoryResponse
-from api.endpoints.user import User, get_current_user
+from api.endpoints.schemas import HistoryResponse
+from api.endpoints.user import get_current_user
 from api.db.history import history_db, HistoryDbException, InvalidUserIdException
 
 history_router = APIRouter(prefix="/history", tags=[""])
@@ -12,13 +15,34 @@ history_router = APIRouter(prefix="/history", tags=[""])
 
 @history_router.get("/")
 async def get_history_from_user_id(
-    user: Annotated[str, Depends(get_current_user)]
+    user: Annotated[str, Depends(get_current_user)],
 ) -> HistoryResponse:
+    """Returns a list of user's history"""
     try:
         history = history_db.get_all_history_by_user_id(user.id)
-        return HistoryResponse(history=history)
-    except InvalidUserIdException as e:
-        raise HTTPException(status_code=404, detail="User not found")
-    except HistoryDbException as e:
+        return history
+    except InvalidUserIdException as error:
+        raise HTTPException(status_code=404, detail="User not found") from error
+    except HistoryDbException as error:
         # Server DB error
-        raise HTTPException(status_code=500)
+        raise HTTPException(status_code=500) from error
+
+
+Page = Page.with_custom_options(
+    size=Field(20, ge=1, le=500),
+)
+
+
+@history_router.get("/paged")
+async def get_history_from_user_id_paged(
+    user: Annotated[str, Depends(get_current_user)],
+) -> Page[HistoryResponse]:
+    """Returns a paginated list of user's history"""
+    try:
+        history = history_db.get_all_history_by_user_id(user.id)
+        return paginate(history)
+    except InvalidUserIdException as error:
+        raise HTTPException(status_code=404, detail="User not found") from error
+    except HistoryDbException as error:
+        # Server DB error
+        raise HTTPException(status_code=500) from error
